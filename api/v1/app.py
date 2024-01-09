346f3184058acb328da606352aaf34368568e6ee
Edmund Eli Bansah <edmundbansah@gmail.com>
2024-01-09 09:01:53 +0000

    Change doc string style for module III

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,58 +1,34 @@
 #!/usr/bin/python3
 """
-    app for strating flask.
-"""
+module: api/v1/app
 
-from flask import Flask, jsonify, make_response
+App logic for API
+"""
+import os
+from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-import os
-from flask_cors import CORS
 
 app = Flask(__name__)
-
-app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-app_port = int(os.getenv('HBNB_API_PORT', '5000'))
-app.url_map.strict_slashes = False
-# Register blueprint
-app.register_blueprint(app_views, url_prefix="/api/v1")
-# CORS
-CORS(app, resources={'/*': {'origins': app_host}})
+app.register_blueprint(app_views)
+CORS(app, origins="0.0.0.0")
 
 
 @app.teardown_appcontext
-def teardown_flask(exception):
-    """The Flask app context and event listener. Closes storage."""
+def close_storage(exception):
+    """Close storage."""
     storage.close()
 
 
 @app.errorhandler(404)
-def error_404(error):
-    """ Handles 404 erros. """
-    msg = 'Not found'
+def not_found(error):
+    """Return a 404 error code status if resource not found."""
     return make_response(jsonify({'error': 'Not found'}), 404)
 
-@app.errorhandler(400)
-def error_400(error):
-    """
-    Handles the 400 HTTP error code.
 
-    Args:
-        error: The error object associated with the 400 error.
+if __name__ == "__main__":
+    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.environ.get('HBNB_API_PORT', 5000))
 
-    Returns:
-        A JSON response with an error message and a 400 status code.
-    """
-    msg = 'Bad request'
-    if isinstance(error, Exception) and hasattr(error, 'description'):
-        msg = error.description
-    return jsonify(error=msg), 400
-
-if __name__ == '__main__':
-    app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    app_port = int(os.getenv('HBNB_API_PORT', '5000'))
-
-    # Enable debug mode
-    app.debug = True
-
-    app.run(host=app_host, port=app_port, threaded=True)
+    app.run(host=host, port=port, threaded=True, debug=True)
