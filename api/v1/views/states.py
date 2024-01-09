a37396d76ccc0bd9fbb9dc2d07194e1102808267
Edmund Eli Bansah <edmundbansah@gmail.com>
2024-01-09 01:45:09 +0000

    Import models

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -34,7 +34,7 @@ def get_state(state_id):
     """
     state = storage.get(State, state_id)
     if not state:
-        raise NotFound(description='State not found')
+        abort(404)
     return jsonify(state.to_dict())
 
 @app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
@@ -53,7 +53,7 @@ def delete_state(state_id):
     """
     state = storage.get(State, state_id)
     if not state:
-        raise NotFound(description='State not found')
+        abort(404)
     state.delete()
     storage.save()
     return jsonify({}), 200
@@ -71,12 +71,13 @@ def create_state():
         BadRequest: If the dictionary doesn’t contain the key name.
     """
     if not request.is_json:
-        raise BadRequest(description='Not a JSON')
+        abort(400, description='Not a JSON')
     data = request.get_json()
     if 'name' not in data:
-        raise BadRequest(description='Missing name')
+        abort(400, description='Missing name')
     new_state = State(**data)
-    new_state.save()
+    storage.new(new_state)
+    storage.save()
     return jsonify(new_state.to_dict()), 201
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
@@ -95,11 +96,11 @@ def update_state(state_id):
         BadRequest: If the HTTP body request is not valid JSON.
     """
     state = storage.get(State, state_id)
-    if not state:
-        raise NotFound(description='State not found')
-    if not request.is_json:
-        raise BadRequest(description='Not a JSON')
+    if state is None:
+        abort(404)
     data = request.get_json()
+    if data is None:
+        abort(404, description="Not a JSON")
     for key, value in data.items():
         if key not in ['id', 'created_at', 'updated_at']:
             setattr(state, key, value)
