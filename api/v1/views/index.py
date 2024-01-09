d8d378c7428c2f1589b1912ab7a6e4d6b7451504
Edmund Eli Bansah <edmundbansah@gmail.com>
2024-01-09 10:32:16 +0000

    Pycodestyle fix II

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -23,12 +23,14 @@ def status():
 @app_views.route('/stats', methods=['GET'])
 def stats():
     """Retrieves the number of each objects by type."""
-    stats = {
-        "amenities": storage.count(Amenity),
-        "cities": storage.count(City),
-        "places": storage.count(Place),
-        "reviews": storage.count(Review),
-        "states": storage.count(State),
-        "users": storage.count(User)
+    objs = {
+        'amenities': Amenity,
+        'cities': City,
+        'places': Place,
+        'reviews': Review,
+        'states': State,
+        'users': User
     }
-    return jsonify(stats)
+    for key, value in objs.items():
+        objs[key] = storage.count(value)
+    return jsonify(objs)
