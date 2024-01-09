1a36648dc8a60dc6c7c248065fda097c04378e64
Edmund Eli Bansah <edmundbansah@gmail.com>
2024-01-09 20:58:24 +0000

    Pep8 fix II

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -10,7 +10,8 @@ from models.city import City
 from werkzeug.exceptions import NotFound, MethodNotAllowed, BadRequest
 
 
-@app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities', methods=['GET'],
+                 strict_slashes=False)
 def get_cities_by_state(state_id):
     """
     Retrieves the list of all City objects of a State.
@@ -74,7 +75,8 @@ def delete_city(city_id):
     return jsonify({}), 200
 
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities', methods=['POST'],
+                 strict_slashes=False)
 def create_city(state_id):
     """
     Creates a City object.
